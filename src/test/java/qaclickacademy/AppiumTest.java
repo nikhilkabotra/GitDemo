b3abff35878e321f291e5c8f51b7e6f5b0ce1d99
nikhil <nikhilkabotra@gmail.com>
2020-01-02 20:16:14 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void AndroidTest()
+	{
+		System.out.println("AndroidTest");
+	}
+	
+	@Test
+	public void IosTest()
+	{
+		System.out.println("IosTest");
+	}
+
+}
